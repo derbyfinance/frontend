7774528578273dfe59dda01f88a21c00248724c0
ramon.klanke <info@inhousemedia.nl>
2023-05-17 10:59:08 +0200

    initial setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+export default {
+    productionBrowserSourceMaps: true,
+    onDemandEntries: {
+        maxInactiveAge: 1000 * 60 * 60,
+        pagesBufferLength: 5
+    },
+    trailingSlash: false,
+    reactStrictMode: true,
+    swcMinify: true,
+}
