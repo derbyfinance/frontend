5f868128dbca4de72764897f40e53937b65b96ac
DolanPump <nickvanrossenberg@hotmail.com>
2023-06-15 09:08:28 +0200

    format

--- src/models/dto/VaultListDtoModel.tsx
+++ src/models/dto/VaultListDtoModel.tsx
@@ -1,6 +1,6 @@
 import { VaultDtoModel } from './VaultDtoModel'
 
 export default interface VaultListDtoModel {
-  count: number
-  results: VaultDtoModel[]
+	count: number
+	results: VaultDtoModel[]
 }
