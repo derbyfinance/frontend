12b9e6d97cef00c9ab9b1ec8b3c7c8eb96f152ab
Theezr <nickvanrossenberg@hotmail.com>
2023-06-20 10:27:14 +0200

    derby token balance hook

---
+++ src/hooks/UseDerbyTokenBalance.tsx
@@ -0,0 +1,14 @@
+import { useAccount, useBalance } from 'wagmi'
+
+const useDerbyTokenBalance = () => {
+	const { address } = useAccount()
+	const { data } = useBalance({
+		address: address,
+		token: process.env.NEXT_PUBLIC_DERBY_TOKEN as `0x${string}`,
+		watch: true
+	})
+
+	return data?.formatted ? Number(data.formatted) : 0
+}
+
+export default useDerbyTokenBalance
