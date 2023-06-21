0ebd9205de48abbade049206a6243e928d1df4ef
ramon.klanke <info@inhousemedia.nl>
2023-06-21 21:28:01 +0200

    Code formatting

--- src/functions/StringFunction.tsx
+++ src/functions/StringFunction.tsx
@@ -1,4 +1,6 @@
-export const MaskCoinAddress = (address?: `0x${string}`): string => {
+import { Hex } from 'viem'
+
+export const MaskCoinAddress = (address?: Hex): string => {
 	return address?.toString().slice(0, 6) + '...' + address?.slice(-4)
 }
 
--- src/models/internal/CountdownModel.tsx
+++ src/models/internal/CountdownModel.tsx
@@ -1,4 +1,4 @@
-export interface CountdownModel {
+export default interface CountdownModel {
 	days: number
 	hours: number
 	minutes: number
