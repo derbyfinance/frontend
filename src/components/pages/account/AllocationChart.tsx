0b152a5dc944e1932b06fbbe188b55668799109c
Ramon Klanke <ramon@tabbby.me>
2023-11-27 20:20:19 +0100

    fix: allocation chart
    sometimes the list is still not fetched from the backend

--- src/components/pages/account/AllocationChart.tsx
+++ src/components/pages/account/AllocationChart.tsx
@@ -1,37 +1,37 @@
 import DoughnutChart from '@components/charts/DoughnutChart'
-import { useAppDispatch, useAppSelector } from '@hooks/ReduxStore'
+import { useAppSelector } from '@hooks/ReduxStore'
 import { PlayerDtoModel } from '@models/dto/PlayerDtoModel'
 import ChartDataModel from '@models/internal/ChartDataModel'
 import { getPlayerState } from '@store/UserSlice'
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { styled } from 'styled-components'
 
 const AllocationChart = () => {
-    const player = useAppSelector<PlayerDtoModel | undefined>(getPlayerState)
-    const [allocationList, setAllocationList] = useState<ChartDataModel[]>([])
-    
-    useEffect(() => { 
-        if (player && player?.player?.baskets.length > 0) { 
-            const allocations: string[] = player.player.baskets[0].allocations
-    
-            const list:ChartDataModel[] = player.player.baskets[0].vault.protocols.map((protocol) => {
+	const player = useAppSelector<PlayerDtoModel | undefined>(getPlayerState)
+	const [allocationList, setAllocationList] = useState<ChartDataModel[]>([])
 
-                const amount = Number(allocations[Number(protocol.number)])
-                return {
-                    label: protocol.name,
-                    data: amount
-                }
-            })
+	useEffect(() => {
+		if (player && player?.player?.baskets.length > 0) {
+			const allocations: string[] = player.player.baskets[0].allocations
 
-            setAllocationList(list.filter(item => item.data > 0))
-        }
-    }, [player])
-    
-    return (
-        <Chart>
-            <DoughnutChart data={allocationList} />
-        </Chart>
-   )
+			const list: ChartDataModel[] =
+				player.player.baskets[0].vault?.protocols?.map((protocol) => {
+					const amount = Number(allocations[Number(protocol.number)])
+					return {
+						label: protocol.name,
+						data: amount
+					}
+				})
+
+			setAllocationList(list.filter((item) => item.data > 0))
+		}
+	}, [player])
+
+	return (
+		<Chart>
+			<DoughnutChart data={allocationList} />
+		</Chart>
+	)
 }
 
 const Chart = styled.div`
